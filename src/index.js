63d231906414e2bd0c4a6ad7153504856be0ea13
Michael Shields <mr.m.p.shields+coding@gmail.com>
2021-12-24 15:43:45 +0000

    switch to using ThemeContext

--- src/index.js
+++ src/index.js
@@ -3,25 +3,25 @@ import ReactDOM from "react-dom"
 
 /* === THEME-CONTEXT === */
 
-// import ThemeContext from "./theme-context/themeContext"
-// import App from "./theme-context/App"
+import ThemeContext from "./theme-context/themeContext"
+import App from "./theme-context/App"
 
-// ReactDOM.render(
-//     <ThemeContext.Provider value={"light"}>
-//       <App />
-//     </ThemeContext.Provider>
-//     , document.getElementById("root")
-// )
+ReactDOM.render(
+    <ThemeContext.Provider value={"light"}>
+      <App />
+    </ThemeContext.Provider>
+    , document.getElementById("root")
+)
 
 
 /* === USERNAME-CONTEXT === */
 
-import UsernameContext from './username-context/usernameContext'
-import App from './username-context/App'
+// import UsernameContext from './username-context/usernameContext'
+// import App from './username-context/App'
 
-ReactDOM.render(
-  <UsernameContext.Provider value={'Smithers'}>
-    <App />
-  </UsernameContext.Provider>
-  , document.getElementById("root")
-)
+// ReactDOM.render(
+//   <UsernameContext.Provider value={'Smithers'}>
+//     <App />
+//   </UsernameContext.Provider>
+//   , document.getElementById("root")
+// )
