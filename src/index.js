adbb677003367f2f76c0b930ad66daece1a13e0c
Michael Shields <mr.m.p.shields+coding@gmail.com>
2021-12-30 17:21:44 +0000

    refactor: place context api folders into one directory, refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,49 +1,38 @@
 import React from "react"
 import ReactDOM from "react-dom"
+
+/* === THEME-CONTEXT === */
+// import { ThemeContextProvider } from "./context-api/theme-context/themeContext"
+// import App from "./context-api/theme-context/App"
+
+/* === USER-CONTEXT === */
+// import { UserContextProvider } from './context-api/user-context/userContext'
+// import App from '.context-api/user-context/App'
+
+/* === USERNAME-CONTEXT === */
+// import UsernameContext from './context-api/username-context/usernameContext'
+// import App from './context-api/username-context/App'
+
+/* === REACT-ROUTER === */
 import App from "./react-router/App"
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
+  // <ThemeContextProvider>
+  //     <App />
+  // </ThemeContextProvider>
+  
+  // <UserContextProvider>
+  //     <App />
+  // </UserContextProvider>
+
+  // <UsernameContext.Provider>
+  //     <App />
+  // </UsernameContext.Provider>
+
   <Router>
     <App />
   </Router>
 , document.getElementById("root"))
 
-
-/* === USER-CONTEXT === */
-
-// import { UserContextProvider } from './user-context/userContext'
-// import App from './user-context/App'
-
-// ReactDOM.render(
-//   <UserContextProvider>
-//     <App />
-//   </UserContextProvider>
-//   , document.getElementById("root")
-// )
-
-/* === THEME-CONTEXT === */
-
-// import { ThemeContextProvider } from "./theme-context/themeContext"
-// import App from "./theme-context/App"
-
-// ReactDOM.render(
-//     <ThemeContextProvider value={"light"}>
-//       <App />
-//     </ThemeContextProvider>
-//     , document.getElementById("root")
-// )
-
-
-/* === USERNAME-CONTEXT === */
-
-// import UsernameContext from './username-context/usernameContext'
-// import App from './username-context/App'
-
-// ReactDOM.render(
-//   <UsernameContext.Provider value={'Smithers'}>
-//     <App />
-//   </UsernameContext.Provider>
-//   , document.getElementById("root")
-// )
 
